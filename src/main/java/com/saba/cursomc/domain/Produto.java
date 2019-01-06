c8e1d9e78688c1b12f7fe367a7c5ed2e5d552da9
Jorge Saba <jgsaba3@gmail.com>
2019-01-06 16:36:04 -0200

    ItemPedido e ItemPedidoPK

--- src/main/java/com/saba/cursomc/domain/Produto.java
+++ src/main/java/com/saba/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.saba.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -33,8 +36,21 @@ public class Produto implements Serializable{
 	)
 	private List<Categoria> categorias = new ArrayList<Categoria>();
 	
+	@OneToMany(mappedBy="id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Produto() {
 		
+	}
+	
+	public List<Pedido> getPedidos(){
+		
+		List<Pedido> lista = new ArrayList<Pedido>();
+		for(ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		
+		return lista;
 	}
 
 	public Produto(Integer id, String nome, Double preco) {
@@ -99,6 +115,14 @@ public class Produto implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 	
 	
---
+++ src/main/java/com/saba/cursomc/repository/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package com.saba.cursomc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.saba.cursomc.domain.ItemPedido;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+}
+
